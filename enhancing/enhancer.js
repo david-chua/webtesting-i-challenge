bc644593cab11d3d0e14a2e1daa8d70ceb54b43a
david-chua <dp_chua@hotmail.com>
2019-04-10 22:38:15 -0400

    finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,14 +6,31 @@ module.exports = {
 };
 
 function succeed(item) {
+  if (item.enhancement === 20){
+    item.enhancement = 20
+  } else {
+    item.enhancement = item.enhancement + 1;
+  }
   return { ...item };
 }
 
+
 function fail(item) {
+  if (item.enhancement < 15){
+    item.durability = item.durability - 5;
+  }
+  else if (item.enhancement >= 15){
+    if (item.enhancement > 16){
+      item.enhancement = item.enhancement - 1;
+    }
+    item.durability = item.durability - 10;
+  }
   return { ...item };
 }
 
 function repair(item) {
+  item.durability = 100;
+
   return { ...item };
 }
 
